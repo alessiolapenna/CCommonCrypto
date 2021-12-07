b1b4e18f0a30947b5d6a6ffe36ea3c0bfef6ac4d
alessiolapenna <alessio.lapenna@gmail.com>
2021-12-07 10:12:55 +0900

    product

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 let package = Package(
     name: "CCommonCrypto",
     platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CCommonCrypto",
+            targets: ["CCommonCrypto"])
+    ],
     targets: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .target(name: "CCommonCrypto")
