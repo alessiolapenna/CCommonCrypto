4a0e80bbeaccf7603c2bab12f667ae8befc16615
alessiolapenna <alessio.lapenna@gmail.com>
2021-12-07 09:01:30 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,22 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "CCommonCrypto"
+    name: "CCommonCrypto",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CCommonCrypto",
+            targets: ["CCommonCrypto"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "CCommonCrypto",
+            dependencies: [])
+    ]
 )
